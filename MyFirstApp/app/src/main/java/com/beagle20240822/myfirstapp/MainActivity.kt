b456aaa38ed3c7ef9b14abdb73398b9a29d79a5a
bruno27866 <bamaral2.git2023@gmail.com>
2024-08-22 14:12:45 -0400

    update: created function 'click'

--- MyFirstApp/app/src/main/java/com/beagle20240822/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/beagle20240822/myfirstapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.beagle20240822.myfirstapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun click(view: View){
+        println("Button was clicked!!!")
     }
 }
